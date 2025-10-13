0448b007308b221e268b5174312dd4813fd8321d
Sofia Grijalva <condorcoders@gmail.com>
2025-10-14 00:07:58 +1000

    Creación de proyecto con Atro y funcionalidad de editor
    - Agregamos fondo
    - Agregamos logo
    - La base de la guagua
    - Funcionalidad de editor para seleccionar diferentes sombreros y outfits

---
+++ src/data/hats.js
@@ -0,0 +1,12 @@
+export const hats = [
+  {
+    id: "flowers",
+    image: "/guagua/hats/flowers.png",
+    top: 0,
+  },
+  {
+    id: "chef-hat",
+    image: "/guagua/hats/chef-hat.png",
+    top: -100,
+  },
+];
